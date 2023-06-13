1030e39abf7e639743797588e671a64819309390
Giovanni Bortolussi <gbortolussi@degreed.com>
2023-06-13 18:51:37 +0200

    latest changes

--- tap_esco/streams.py
+++ tap_esco/streams.py
@@ -3,11 +3,13 @@
 import logging
 import requests
 import re
+import pandas as pd
 
 from urllib.request import urlopen
-from typing import Optional, Iterable
+from typing import Optional, Iterable, Dict, Any
 from singer_sdk import typing as th
 from singer_sdk.streams import RESTStream
+from singer_sdk.helpers.jsonpath import extract_jsonpath
 
 logging.basicConfig(level=logging.INFO)
 
@@ -21,6 +23,20 @@ class TapEscoStream(RESTStream):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.columns = [
+            "uri",
+            "title",
+            "description_en",
+            "alternativeLabel_en",
+            "selectedVersion",
+            "uri_level_0",
+            "title_level_0",
+            "uri_level_1",
+            "title_level_1",
+            "uri_level_2",
+            "title_level_2",
+        ]
+        self.results_df = pd.DataFrame(columns=self.columns)
         # Getting last available ESCO version scraping ESCO website
         esco_url = "https://esco.ec.europa.eu/en/classification/skill_main"
         start_string = '<div class="block-wrapper--esco_version">'
@@ -45,45 +61,121 @@ class EscoSkillsDetails(TapEscoStream):
         base_uri=base_uri
     )  # API endpoint after base_url
     primary_keys = ["uri"]
-    replication_key = "selectedVersion"
+    records_jsonpath = "$[*]"  # https://jsonpath.com Use requests response json to identify the json path
+    # replication_key = "selectedVersion"
     schema = th.PropertiesList(
-        th.Property("uri", th.StringType), th.Property("selectedVersion", th.StringType)
+        th.Property("uri", th.StringType),
+        th.Property("title", th.StringType),
+        th.Property("description_en", th.StringType),
+        th.Property("alternativeLabel_en", th.StringType),
+        th.Property("selectedVersion", th.StringType),
+        th.Property("uri_level_0", th.StringType),
+        th.Property("title_level_0", th.StringType),
+        th.Property("uri_level_1", th.StringType),
+        th.Property("title_level_1", th.StringType),
+        th.Property("uri_level_2", th.StringType),
+        th.Property("title_level_2", th.StringType),
     ).to_dict()
+
+    def _create_dataframe(self, response):
+        # uri
+        uri = response["uri"]
+        # title
+        if "title" in response:
+            title = response["title"]
+        else:
+            title = ""
+        # description_en
+        if "description" in response:
+            if "en-us" in response["description"]:
+                description_en = response["description"]["en-us"]["literal"]
+            else:
+                description_en = ""
+        else:
+            description_en = ""
+        # alternativeLabel
+        if "alternativeLabel" in response:
+            if "en" in response["alternativeLabel"]:
+                alternativeLabel_en = " | ".join(response["alternativeLabel"]["en"])
+            else:
+                alternativeLabel_en = ""
+        else:
+            alternativeLabel_en = ""
+        # uri_level_2
+        # title_level_2
+        # uri_level_1
+        # title_level_1
+        # uri_level_0
+        # title_level_0
+
+        new_row = pd.DataFrame(
+            [
+                {
+                    "uri": uri,
+                    "title": title,
+                    "description_en": description_en,
+                    "alternativeLabel_en": alternativeLabel_en,
+                    "selectedVersion": self.selectedVersion,
+                    "uri_level_2": "",
+                    "title_level_2": "",
+                    "uri_level_1": "",
+                    "title_level_1": "",
+                    "uri_level_0": "",
+                    "title_level_0": "",
+                }
+            ]
+        )
+        self.results_df = pd.concat(
+            [self.results_df, new_row], axis=0, ignore_index=True
+        )
 
     def _get_skills_details(self, uris_list):
         uris = ",".join(uris_list)
         response = requests.get(self.url_base + "/resource/concept?uris=" + uris)
         if response.status_code == 200:
             logging.info("Parsed {len} skills".format(len=len(uris_list)))
+            for uri in response.json()["_embedded"]:
+                self._create_dataframe(response.json()["_embedded"][uri])
+
         else:
             logging.warning(
-                "Error found while running bulk operation. Processing skills one by one."
+                "Error found while running bulk operation. Processing skills one by one"
             )
             for uri in uris_list:
                 response = requests.get(self.url_base + "/resource/concept?uri=" + uri)
                 if response.status_code == 200:
                     logging.info("Parsed uri: {uri}".format(uri=uri))
+                    self._create_dataframe(response.json())
                 else:
-                    logging.warning("Error found in uri: {uri}".format(uri=uri))
+                    logging.warning("Error found at url: {}".format(response.url))
                     continue
 
-    def _get_response(self, response):
-        logging_text = "Skills taxonomy level {}"
-
-        if response.status_code == 200:
+    def _get_uris(self, response, i):
+        if response.status_code == 200 and response.json():
             if "narrowerConcept" in response.json()["_links"]:
                 for narrowerConcept in response.json()["_links"]["narrowerConcept"]:
-                    logging.info(logging_text.format(narrowerConcept["uri"]))
-                    response = requests.get(
-                        self.url_base
-                        + "/resource/concept?uri="
-                        + narrowerConcept["uri"]
-                    )
-                    response = self._get_response(response)
+                    uri = narrowerConcept["uri"]
+                    title = narrowerConcept["title"]
+                    if uri.startswith("http://data.europa.eu/esco/skill/S7"):
+                        logging.info(i)
+                        i += 1
+                        if i > 2:
+                            i = 2
+                        logging.info("Skills taxonomy level uri: {}".format(uri))
+                        response = requests.get(
+                            self.url_base + "/resource/concept?uri=" + uri
+                        )
+                        response = self._get_uris(response, i)
+                    else:
+                        logging.info(
+                            "##### Skipping skills taxonomy level uri: {}".format(uri)
+                        )
             elif "narrowerSkill" in response.json()["_links"]:
                 uris_list = []
                 narrowerSkills = response.json()["_links"]["narrowerSkill"]
                 logging.info("Found {} skills".format(len(narrowerSkills)))
+                i = 0
+                logging.warn(i)
                 for narrowerSkill in narrowerSkills:
                     uris_list.append(narrowerSkill["uri"])
                     if len(uris_list) == 50:
@@ -94,22 +186,10 @@ class EscoSkillsDetails(TapEscoStream):
         else:
             logging.warning("Error found at url: {}".format(response.url))
 
-    def _get_uris(self, response):
-        level_0 = self._get_response(response)
-
     def parse_response(self, response: requests.Response) -> Iterable[dict]:
         """Parse the response and return an iterator of result records."""
         if response.json():
-            uris_list = self._get_uris(response)
-
-    # https://sdk.meltano.com/en/latest/parent_streams.html
-    def get_child_context(self, record: dict, context: Optional[dict]) -> dict:
-        """Return a context dictionary for child streams."""
-        if "replication_key_value" in self.stream_state:
-            if self.selectedVersion == self.stream_state["replication_key_value"]:
-                logging.info("ESCO version is up to date. Exiting.")
-                exit()
-            else:
-                return record
-        else:
-            return record
+            self._get_uris(response, 0)
+            logging.info("Total number of skills: {}".format(len(self.results_df)))
+            input = self.results_df.to_dict()
+            yield from extract_jsonpath(self.records_jsonpath, input=input)
